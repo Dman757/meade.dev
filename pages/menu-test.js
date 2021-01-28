3ee21ea515be8c771299e87de6708e8fb3137511
David Meade <dman757@gmail.com>
2021-01-27 23:08:19 -0800

    I guess nextjs doesn't like fat arrow pages

--- pages/menu-test.js
+++ pages/menu-test.js
@@ -1,11 +1,11 @@
 import PageLayout from 'components/PageLayout.js';
 import CircleMenu from 'components/CircleMenu';
 
-export default MenuTest = () => {
+export default function MenuTest() {
   return (
     <PageLayout>
       <h3>Card Elements</h3>
       <CircleMenu />
     </PageLayout>
   );
-};
+}
